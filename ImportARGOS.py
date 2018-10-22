6495ea5bbd9192243d2e1d68954bb43bac2dbc93
wrb15 <42747406+wrb15@users.noreply.github.com>
2018-10-22 11:18:47 -0400

    Gets tags and coordinates

--- ImportARGOS.py
+++ ImportARGOS.py
@@ -16,3 +16,38 @@ import sys, os, arcpy
 # Set input variables (Hard-wired)
 inputFile = 'W:/wrb15/ARGOSTracking/Data/ARGOSData/1997dg.txt'
 outputFC = "W:/wrb15/ARGOSTracking/Scratch/ARGOStrack.shp"
+
+# Open the ARGOS data file for reading
+inputFileObj = open(inputFile, 'r')
+
+# Get the first line of data, so we can use a while loop
+lineString = inputFileObj.readline()
+while lineString:
+
+    # Set code to run only if the line contains the string "Date: "
+    if ("Date :" in lineString):
+        
+        # Parse the line into a list
+        lineData = lineString.split()
+        
+        # Extract attributes from the datum header line
+        tagID = lineData[0]
+        
+        # Extract location info from the next line
+        line2String = inputFileObj.readline()
+        
+        # Parse the line into a list
+        line2Data = line2String.split()
+        
+        # Extract the date we need to variables
+        obsLat = line2Data[2]
+        obsLon= line2Data[5]
+        
+        # Print results to see how we're doing
+        print (tagID,"Lat:"+obsLat,"Long:"+obsLon)
+        
+    # Move to the next line so the while loop progresses
+    lineString = inputFileObj.readline()
+    
+#Close the file object
+inputFileObj.close()
